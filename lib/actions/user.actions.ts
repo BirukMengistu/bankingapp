ed044925ab12ea8298ed6ba82a3b71ebf7010e97
Birukmengistu <sheger.shelf3@gmail>
2024-10-01 14:32:41 +0200

    auth and login

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,26 +1,87 @@
 'use server'
 
-export const signIn= async (data:signInProps) =>{
+import { ID, Query } from "node-appwrite"
+import { createAdminClient, createSessionClient } from "../appwrite"
+import { cookies } from "next/headers"
+import { parseStringify } from "../utils"
+const {
+APPWRITE_DATABASE_ID: DATABASE_ID,
+APPWRITE_USER_COLLECTION_ID: USER_COLLECTION_ID,
+APPWRITE_BANK_COLLECTION_ID: BANK_COLLECTION_ID,
+} = process.env;
 
-    try{
+export const getUserInfo = async ({ userId }: getUserInfoProps) => {
+    try {
+      const { database } = await createAdminClient();
+  
+      const user = await database.listDocuments(
+        DATABASE_ID!,
+        USER_COLLECTION_ID!,
+        [Query.equal('userId', [userId])]
+      )
+  
+      return parseStringify(user.documents[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+export const signIn = async (data: signInProps) => {
+
+    try {
         //mutations
         return data
     }
-    catch (err)
-    {
+    catch (err) {
         console.error(err)
     }
 }
 
 
-export const signUp = async (data:signInProps) =>{
+export const signUp = async ({ password, ...userData }: SignUpParams) => {
 
-    try{
-        //mutations
-        return data
+
+    const { email, firstName, lastName } = userData;
+
+    try {
+        const { account } = await createAdminClient();
+
+        const newUserAccount = await account.create(
+            ID.unique(),
+             userData.email,
+             password,
+            `${firstName} ${lastName}`);
+    
+            if(!newUserAccount) throw new Error('Error creating user')
+
+        const session = await account.createEmailPasswordSession(email, password);
+
+        cookies().set("appwrite-session", session.secret, {
+            path: "/",
+            httpOnly: true,
+            sameSite: "strict",
+            secure: true,
+        });
+
+        return parseStringify(newUserAccount);
     }
-    catch (err)
-    {
+    catch (err) {
         console.error(err)
     }
 }
+
+// ... your initilization functions
+
+export async function getLoggedInUser() {
+    try {
+      const { account } = await createSessionClient();
+      const result = await account.get();
+  
+      const user = await getUserInfo({ userId: result.$id})
+  
+      return parseStringify(user);
+    } catch (error) {
+      console.log(error)
+      return null;
+    }
+  }
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,10 +1,43 @@
-import { Client, Account } from 'appwrite';
+"use server";
 
-export const client = new Client();
+import { Client, Account, Databases, Users } from "node-appwrite";
+import { cookies } from "next/headers";
 
-client
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('<PROJECT_ID>'); // Replace with your project ID
+export async function createSessionClient() {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
 
-export const account = new Account(client);
-export { ID } from 'appwrite';
+  const session = cookies().get("appwrite-session");
+
+  if (!session || !session.value) {
+    throw new Error("No session");
+  }
+
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+  };
+}
+
+export async function createAdminClient() {
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
+    .setKey(process.env.NEXT_APPWRITE_KEY!);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get database() {
+      return new Databases(client);
+    },
+    get user() {
+      return new Users(client);
+    }
+  };
+}
